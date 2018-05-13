2f091fabbe1cd92be22ad9423f72f008c63eba84
Leandro Coello <ln.coello8@gmail.com>
2018-05-13 15:23:45 -0300

    First Demo

--- Tuner/app/src/main/java/com/example/leo/tunner/MainActivity.java
+++ Tuner/app/src/main/java/com/example/leo/tunner/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.leo.tunner;
 
+import android.Manifest;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.leo.tunner.Activity.Tuner;
+import com.example.leo.tunner.R;
+import com.example.leo.tunner.Task.ProcessingTaskDemo;
+
 import java.util.ArrayList;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,23 +23,52 @@ public class MainActivity extends AppCompatActivity {
 
     private Button sixStringsGuitar, eightStringsGuitar, fourStringsBass, sixStringsBass;
 
+    // STARTS DEMO
+
+    private boolean isListeningDemo;
+    private ProcessingTaskDemo ptask;
+    private Button strtButton, stpButton;
+    private TextView textFr;
+
+
+    // ENDS DEMO
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
 
-        Configuration conf = this.getResources().getConfiguration();
 
-        if(conf.orientation ==Configuration.ORIENTATION_PORTRAIT){
+        //        Configuration conf = this.getResources().getConfiguration();
+//
+//        if(conf.orientation ==Configuration.ORIENTATION_PORTRAIT){
+//
+//            setContentView(R.layout.activity_main);
+//        }else {
+//            setContentView(R.layout.activity_main_landscape);
+//
+//        }
 
-            setContentView(R.layout.activity_main);
-        }else {
-            setContentView(R.layout.activity_main_landscape);
+//        initialize();
 
+//        STARTS DEMO
+
+        setContentView(R.layout.demo);
+        strtButton = (Button) findViewById(R.id.start);
+        strtButton.setEnabled(true);
+        stpButton = (Button) findViewById(R.id.stop);
+        stpButton.setEnabled(false);
+        textFr = (TextView) findViewById(R.id.freq);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                    123);
         }
 
-        initialize();
+//        ENDS DEMO
 
 
     }
@@ -114,5 +147,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //STARTS DEMO
+
+    public void startRecording(View v){
+
+        strtButton.setEnabled(false);
+        stpButton.setEnabled(true);
+        isListeningDemo = true;
+
+        ptask = new ProcessingTaskDemo(this);
+        ptask.execute();
+
+
+    }
+
+    public void stopRecording(View v){
+        isListeningDemo = false;
+        strtButton.setEnabled(true);
+        stpButton.setEnabled(false);
+        textFr.setText(" ");
+    }
+
+    public boolean isListeningDemo() {
+        return isListeningDemo;
+    }
+
+    public void setListeningDemo(boolean listeningDemo) {
+        isListeningDemo = listeningDemo;
+    }
+
+    public void updateTxtView(String text) {
+
+        textFr.setText(text);
+    }
+
+    //ENDS DEMO
 
 }
---
+++ Tuner/app/src/main/java/com/example/leo/tunner/Task/ProcessingTaskDemo.java
@@ -0,0 +1,110 @@
+package com.example.leo.tunner.Task;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+
+import com.example.leo.tunner.MainActivity;
+import com.example.leo.tunner.PitchRecognitionPack.FastYin;
+import com.example.leo.tunner.PitchRecognitionPack.PitchDetectionResult;
+
+public class ProcessingTaskDemo extends AsyncTask<Float, Float, Float> {
+
+
+    private static short[] samples;
+    private static final int SAMPLE_RATE = 44100;// 48000;
+    private static final int NUMBER_OF_SAMPLES = 5120;
+    private static int N;
+
+
+    MainActivity mainActivity;
+
+    public ProcessingTaskDemo(MainActivity t){
+        mainActivity = t;
+    }
+
+
+    @Override
+    protected Float doInBackground(Float... params) {
+
+        //McLeodPitchMethod mpm = new McLeodPitchMethod(SAMPLE_RATE, NUMBER_OF_SAMPLES);
+        //AMDF amdf = new AMDF(SAMPLE_RATE, NUMBER_OF_SAMPLES);
+        //Yin yinM = new Yin(SAMPLE_RATE, NUMBER_OF_SAMPLES);
+        //DynamicWavelet dw = new DynamicWavelet(SAMPLE_RATE, NUMBER_OF_SAMPLES);
+        FastYin fy = new FastYin(SAMPLE_RATE, NUMBER_OF_SAMPLES);
+
+        N = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        samples = new short[NUMBER_OF_SAMPLES];
+
+        AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.VOICE_RECOGNITION, SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, N * 10);
+        recorder.startRecording();
+
+
+
+        while(mainActivity.isListeningDemo()) {
+
+            recorder.read(samples, 0, samples.length);
+            //float[] infsamples = increaseGain(shortToFloat(samples),3f);
+            float[] infsamples = shortToFloat(samples);
+
+            //PitchDetectionResult pr = mpm.getPitch(infsamples); //Bad Results
+            //PitchDetectionResult pr = amdf.getPitch(infsamples); //Bad Results
+            //PitchDetectionResult pr = yinM.getPitch(infsamples); //Best Results
+            //PitchDetectionResult pr = dw.getPitch(infsamples); //Bad Results
+            PitchDetectionResult pr = fy.getPitch(infsamples); //Goood Results
+
+
+            Double ad1 = 0.0;
+            Float res = ad1.floatValue();
+                if(pr.isPitched()) {
+                    res = pr.getPitch();
+                }
+                 publishProgress(res);
+
+
+        }
+        recorder.stop();
+        recorder.release();
+        Double ad = 180.0;
+        Float a = ad.floatValue();
+
+        return a;
+    }
+
+
+
+
+    @Override
+    protected void onProgressUpdate(Float... values) {
+        mainActivity.updateTxtView(values[0].toString()+"Hz");
+    }
+
+    @Override
+    protected void onPostExecute(Float result) {
+        mainActivity.updateTxtView(" ");
+    }
+
+    public float[] shortToFloat(short[] samples){
+        float[] converted = new float[samples.length];
+
+        for(int i = 0; i< samples.length; i++){
+
+            Short n = samples[i];
+
+            converted[i] = n.floatValue();
+        }
+
+        return converted;
+    }
+
+    public float[] increaseGain(float [] samples, float gain){
+
+        for (int i=0; i <samples.length; i++){
+            samples[i]= samples[i] * gain;
+        }
+
+        return samples;
+    }
+
+}
